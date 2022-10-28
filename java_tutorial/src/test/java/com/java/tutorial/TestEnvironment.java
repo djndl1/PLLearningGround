a6e26d2692b237eb5e464d4643456235ec609443
DJN <djndl1@gmail.com>
2022-10-28 23:48:28 +0800

    refactor(TestEnvironment): initialize JsonSerializer with JsonMapper

--- java_tutorial/src/test/java/com/java/tutorial/TestEnvironment.java
+++ java_tutorial/src/test/java/com/java/tutorial/TestEnvironment.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class TestEnvironment {
@@ -13,17 +13,12 @@ public class TestEnvironment {
 
     public static ObjectMapper jsonSerializer() {
         if (jsonSerializer == null) {
-            jsonSerializer = new ObjectMapper();
-            jsonSerializer.registerModules(
-                new Jdk8Module(),
-                new JavaTimeModule()
-            );
-
-            jsonSerializer.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-            jsonSerializer.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
-            jsonSerializer.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-            jsonSerializer.enable(SerializationFeature.INDENT_OUTPUT);
+            jsonSerializer = JsonMapper.builder().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                                .disable(MapperFeature.DEFAULT_VIEW_INCLUSION)
+                                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                                .enable(SerializationFeature.INDENT_OUTPUT)
+                                .addModules(new Jdk8Module(), new JavaTimeModule())
+                                .build();
         }
 
         return jsonSerializer;
