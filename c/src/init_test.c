f292997a3bd2bb95720aa458111de0056b5341ca
djndl1 <djndl1@gmail.com>
2023-05-10 21:57:17 +0800

    feat(c): add gnulib unistr conversion

--- c/src/init_test.c
+++ c/src/init_test.c
@@ -1,3 +1,19 @@
+#include "config.h"
 #include "utest.h"
+
+#include "unistr.h"
+
+UTEST(UTF_TEST, UTF8UTF16) {
+    const char *utf8str = "中文";
+
+    uint16_t utf16str[6] = { 0 };
+    size_t utf16length = 6;
+    u8_to_u16(utf8str, strlen(utf8str), utf16str, &utf16length);
+
+    for (size_t i = 0; i < 6; i++) {
+        printf("%X ", utf16str[i]);
+    }
+    putchar('\n');
+}
 
 UTEST_MAIN();
