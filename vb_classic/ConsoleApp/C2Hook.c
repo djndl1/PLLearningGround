68cab9425f9d835669a2d8324dbb133eadb5fb1d
DJN <djndl1@gmail.com>
2023-07-09 20:11:50 +0800

    feat(VBClassic): C2.EXE hook

--- vb_classic/ConsoleApp/C2Hook.c
+++ vb_classic/ConsoleApp/C2Hook.c
@@ -4,7 +4,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BUFFER_LEN 2048
+#define BUFFER_LEN 4096
+
+#define REAL_C2_PATH L"C:/Program Files/Microsoft Visual Studio/VB98/C3.EXE"
+#define REAL_LINK_PATH L"C:/Program Files/Microsoft Visual Studio/VB98/LINK2.EXE"
 
 /**
  * Compose a logger name
@@ -15,52 +18,50 @@ wchar_t *compose_log_name()
     time_t current_timestamp = time(NULL);
     struct tm *local_time = localtime(&current_timestamp);
 
-    char* logname = malloc(BUFFER_LEN);
-    size_t len = strftime(logname,
+    wchar_t *logwname = malloc(sizeof(wchar_t) * BUFFER_LEN);
+    size_t len = wcsftime(logwname,
                           BUFFER_LEN,
-                          "C2_%Y-%m-%dT%H-%M-%S.txt",
+                          L"HOOK_%Y-%m-%dT%H-%M-%S.txt",
                           local_time);
     if (len == 0) {
         goto time_string_error;
     }
 
-    wchar_t *logwname = malloc(2 * BUFFER_LEN);
-    size_t wlen = mbstowcs(logwname, logname, 2 * BUFFER_LEN);
-    if (wlen == -1) {
-        goto conversion_error;
-    }
-
     return logwname;
 
-conversion_error:
-    free(logwname);
 time_string_error:
-    free(logname);
+    free(logwname);
 
-    return L"C2.txt";
+    return L"HOOK.txt";
 }
 
-int main(void)
+int wmain(int argc, wchar_t *argv[])
 {
-    LPWSTR commandline_str = GetCommandLineW();
-    int argc = 0;
-    LPWSTR *argv = CommandLineToArgvW(commandline_str, &argc);
     FILE *output_stream;
     wchar_t c2argv[BUFFER_LEN] = L"";
 
     wchar_t *logfile = compose_log_name();
     if ((output_stream = _wfopen(logfile, L"a")) != NULL) {
         for (int idx = 1; idx < argc; idx++) {
-
-
             wcscat(c2argv, L" ");
             wcscat(c2argv, argv[idx]);
         }
-        fwprintf(output_stream, L"C2.EXE %ls", c2argv);
+        fwprintf(output_stream, L"%ls %ls\n", argv[0], c2argv);
         fclose(output_stream);
 
-        wprintf(L"%ls\n", c2argv);
+        wprintf(L"%ls %ls\n", argv[0], c2argv);
         fflush(stdout);
+
+        wchar_t *proc_path;
+        if (wcsstr(argv[0], L"C2") != NULL) {
+            proc_path = REAL_C2_PATH;
+        } else if (wcsstr(argv[0], L"LINK") != NULL) {
+            proc_path = REAL_LINK_PATH;
+        } else {
+            wprintf(L"Failed to start %ls %ls\n", argv[0], c2argv);
+            return -1;
+        }
+        wprintf(L"%ls %ls\n", proc_path, c2argv);
 
         PROCESS_INFORMATION new_proc;
         STARTUPINFO info = {
@@ -68,7 +69,7 @@ int main(void)
             .lpReserved = NULL,
         };
 
-        BOOL result = CreateProcessW(L"C:/Program Files/Microsoft Visual Studio/VB98/C3.EXE",
+        BOOL result = CreateProcessW(proc_path,
                                      c2argv,
                                      NULL, NULL,
                                      FALSE, 0,
@@ -76,7 +77,7 @@ int main(void)
                                      &info, &new_proc);
 
         if (!result) {
-            wprintf(L"Failed to start C3.EXE %ls\n", c2argv);
+            wprintf(L"Failed to start %ls %ls\n", argv[0], c2argv);
         }
     }
 
