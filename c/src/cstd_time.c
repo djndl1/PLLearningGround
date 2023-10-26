fb1e9985e4889e06ce051e66daacf370f41cc8da
djndl1 <djndl1@gmail.com>
2023-10-26 23:12:22 +0800

    refactor(std/time): add glibc support

--- c/src/cstd_time.c
+++ c/src/cstd_time.c
@@ -4,13 +4,22 @@
 #include "utest.h"
 
 #if defined(__MINGW32__) || defined (_MSC_VER)
-#define localtime_s(a, b) (localtime_s(b, a))
+    #define localtime_s(a, b) (localtime_s(b, a))
+    #define gmtime_s(a, b) (gmtime_s(b, a))
+#elif defined(__STDC_LIB_EXT1__)
+    #define __STDC_WANT_LIB_EXT1__ 1
+#else
+    #define localtime_s(a, b) (localtime_r(a, b))
+    #define gmtime_s(a, b) (gmtime_r(a, b))
 #endif
+
 
 UTEST(DATETIME, NOW_SECONDS) {
     time_t now = time(nullptr);
 
-    printf("Current unix time: %ld seconds since Epoch.\n", now);
+    struct tm broken_down = { 0 };
+    gmtime_s(&now, &broken_down);
+    printf("Current unix time: %lld seconds since Epoch, that is %s.\n", now, asctime(&broken_down));
 }
 
 UTEST(DATETIME, NOW_NANOSECOND) {
@@ -18,8 +27,11 @@ UTEST(DATETIME, NOW_NANOSECOND) {
 
     timespec_get(&now, TIME_UTC);
 
-    printf("Current Unix time: %lf seconds since Epoch\n",
-           now.tv_sec + now.tv_nsec / 1e9);
+    struct tm broken_down = { 0 };
+    gmtime_s(&now.tv_sec, &broken_down);
+    printf("Current Unix time: %lf seconds since Epoch, that is %s\n",
+           now.tv_sec + now.tv_nsec / 1e9,
+           asctime(&broken_down));
 }
 
 UTEST(DATETIME, LOCAL_NOW) {
@@ -32,6 +44,10 @@ UTEST(DATETIME, LOCAL_NOW) {
         char iso[30] = { '\0' };
         strftime(iso, 30, "%Y-%m-%dT%H:%M:%S", &local_now);
         printf("Current Local time: %s.\n", iso);
+
+        time_t now2 = mktime(&local_now);
+
+        ASSERT_EQ(now, now2);
     }
 
     {
@@ -48,8 +64,10 @@ UTEST(DATETIME, LOCAL_NOW) {
         snprintf(iso_nano_string, 32, "%s.%9ld", iso_nano, now_nano.tv_nsec);
 
         printf("Current Local time: %s.\n", iso_nano_string);
-    }
 
+        time_t now2 = mktime(&local_now_nano);
+        ASSERT_EQ(now_nano.tv_sec, now2);
+    }
 }
 
 UTEST_MAIN();
