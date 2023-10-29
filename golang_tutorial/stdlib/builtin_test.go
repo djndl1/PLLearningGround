5c3d9b85c3493bb7db8955722a9b64ab08aa8d6d
djndl1 <djndl1@gmail.com>
2023-10-29 16:38:00 +0800

    learn(golang): builtin functions 1

---
+++ golang_tutorial/stdlib/builtin_test.go
@@ -0,0 +1,22 @@
+package test_learn
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestComplexType(t *testing.T) {
+	c := complex(1.0, 2.0)
+
+	assert.InDelta(t, 1.0, real(c), math.SmallestNonzeroFloat64)
+	assert.InDelta(t, 2.0, imag(c), math.SmallestNonzeroFloat64)
+}
+
+func TestSlices(t *testing.T) {
+	var slis []string
+	slis = append(slis, "a", "b")
+
+	assert.Equal(t, 2, len(slis))
+}
--- golang_tutorial/stdlib/time_test.go
+++ golang_tutorial/stdlib/time_test.go
@@ -1,4 +1,4 @@
-package time_test
+package test_learn
 
 import (
 	"fmt"
