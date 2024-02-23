5c8e0f7d6f75312614ce16dd3203bcd209971e5e
DJN <djndl1@gmail.com>
2024-02-23 17:30:20 +0800

    feat(cpp/c++98): excluded words when counting words

---
+++ cpp/cxx98/exclude_words.cc
@@ -0,0 +1,44 @@
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <map>
+
+using std::cin;
+using std::cout;
+using std::set;
+using std::map;
+using std::string;
+using std::ostream_iterator;
+
+static std::set<std::string> make_excluded_words()
+{
+    string words[] = { "a", "an", "in", "or", "but", "and", "the" };
+
+    return set<string>(words, words + sizeof(words) / sizeof(words[0]));
+}
+
+static set<string> excluded_words = make_excluded_words();
+
+int main()
+{
+    map<string, unsigned> word_counts;
+
+    string read_word;
+    while (cin >> read_word) {
+        if (excluded_words.find(read_word) != excluded_words.end()) {
+            cout << "Try again: " << read_word << " is an excluded word.\n";
+            continue;
+        }
+
+        word_counts[read_word]++;
+    }
+
+    for (map<string, unsigned>::iterator it = word_counts.begin();
+         it != word_counts.end();
+         it++) {
+        cout << it->first << ": " << it->second << '\n';
+    }
+
+    return 0;
+}
