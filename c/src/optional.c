82bc2f6f08d7f77ffd56951b76f1f5531f14e104
djndl1 <djndl1@gmail.com>
2024-03-16 17:24:05 +0000

    refactor(c): convert some macros to functions

--- c/src/optional.c
+++ c/src/optional.c
@@ -1,17 +1,19 @@
-#include "optional.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
 
-def_optional(size_t);
-def_optional(bool);
+#define optional_element_t size_t
+#include "optional.h"
+
+#define optional_element_t bool
+#include "optional.h"
 
 optional(size_t) index_of(const int *arr, size_t len, int member)
 {
     for (size_t i = 0; i < len; i++) {
         if (arr[i] == member) {
-            return optional_some(size_t, i);
+            return optional_some(size_t)(i);
         }
     }
 
@@ -23,7 +25,7 @@ static inline bool as_bool(unsigned char c)
     return c;
 }
 
-bool is_odd(int n)
+bool is_odd(size_t n)
 {
     return !(n % 2);
 }
@@ -33,10 +35,11 @@ int main(void)
     int f[] = { 1, 2, 3, 4, 5, 6, 7 };
     optional(size_t) result = index_of(f, sizeof(f) / sizeof(f[0]), 1);
 
-    optional(bool) found = optional_map(bool, result, as_bool);
+    optional(bool) found = optional_map_to(bool, result, as_bool);
     printf(optional_present(found) ? "Found\n" : "Not found\n");
 
-    optional_match(size_t, result, found_idx_opt,
+    optional_match(result,
+                   size_t, found_idx_opt,
         scoped_stmt(
             printf("Matched\n"),
             printf("Found 1 at %zu\n", found_idx_opt)
@@ -44,11 +47,12 @@ int main(void)
             printf("Not found")
     );
 
-    optional(size_t) filtered = optional_filter(size_t, result, is_odd);
+    optional(size_t) filtered = optional_filter(size_t)(result, is_odd);
     assert(optional_present(filtered));
 
     optional(size_t) found_none = index_of(f, sizeof(f) / sizeof(f[0]), 8);
-    optional_match(size_t, found_none, _,
+    optional_match(found_none,
+                   size_t, _,
             printf("Matched\n"),
             printf("Not found\n")
         );
--- c/src/optional.h
+++ c/src/optional.h
@@ -1,45 +1,74 @@
-#ifndef OPTIONAL_H_
-#define OPTIONAL_H_
+#ifndef MAYBE_OPTIONAL_H
+#define MAYBE_OPTIONAL_H
+
+#include <stdbool.h>
 
 #define TOKENIZE(t) #t
+
 #define _CONCAT(a, b) a##b
+
 #define CONCAT(a, b) _CONCAT(a, b)
 
-#define scoped_stmt(...) do { __VA_ARGS__; } while (0)
+#define scoped_stmt(...) do { __VA_ARGS__; } while (false)
+
 #define empty_expr scoped_stmt((void)0)
 
 #define optional(typ) CONCAT(CONCAT(optional_, typ), _t)
 
-#define def_optional(typ) typedef struct optional(typ) {   \
-        unsigned char _present;                                         \
-        struct {                                                        \
-            typ t_value;                                                \
-        } _hidden_obj;                                                  \
-    } optional(typ)
-
-#define _optional_val(opt) (opt._hidden_obj.t_value)
-
-#define optional_some(typ, val) ((optional(typ)){ ._present = 1, ._hidden_obj.t_value = val, })
-#define optional_none(typ) ((optional(typ)){ ._present = 0 })
+#define OPTIONAL_INLINE __attribute__ ((always_inline))
 
 #define optional_present(opt) (opt._present)
 #define optional_empty(opt) (!opt._present)
 #define optional_or_else(opt, alt) (opt._present ? _optional_val(opt) : alt)
 
-#define optional_map(D, opt, mapping) (opt._present ?                \
-                                          optional_some(D, mapping(_optional_val(opt))) \
-                                          : optional_none(D))
-#define optional_filter(T, opt, pred) ((opt._present) ? \
-                                         (pred(_optional_val(opt)) ? opt : optional_none(T)) \
-                                       : optional_none(T))
+#define optional_some(T) CONCAT(optional(T), _some)
+#define optional_filter(T) CONCAT(optional(T), _filter)
+#define optional_none(T) ((optional(T)){ ._present = false })
+#define optional_element_predicate(T) CONCAT(optional(T), _element_predicate_t)
 
-#define optional_match(typ, opt, val_var, matched_stmt, empty_stmt) do { \
+#define optional_match(opt, typ, val_var, matched_stmt, empty_stmt) do { \
         if (optional_present(opt)) {                                    \
             typ val_var = _optional_val(opt);                           \
             scoped_stmt(matched_stmt);                                  \
         } else {                                                        \
             empty_stmt;                                                \
                 }                                                      \
-    } while (0)
+    } while (false)
 
-#endif // OPTIONAL_H_
+#define _optional_val(opt) (opt._hidden_obj.t_value)
+
+#endif // MAYBE_OPTIONAL_H
+
+#ifdef optional_element_t
+
+typedef struct optional(optional_element_t) {
+    bool _present;
+    struct {
+        optional_element_t t_value;
+    } _hidden_obj;
+} optional(optional_element_t);
+
+OPTIONAL_INLINE
+static inline optional(optional_element_t) optional_some(optional_element_t)(optional_element_t val)
+{
+    return ((optional(optional_element_t)){ ._present = true, ._hidden_obj.t_value = val, });
+}
+
+#define optional_map_to(U, opt, mapping) (optional_present(opt) ?           \
+                                       optional_some(U)(mapping(_optional_val(opt))) \
+                                       : optional_none(U))
+
+typedef bool (*optional_element_predicate(optional_element_t))(optional_element_t);
+
+OPTIONAL_INLINE
+static inline optional(optional_element_t) optional_filter(optional_element_t)(
+    optional(optional_element_t) opt,
+    optional_element_predicate(optional_element_t) pred)
+{
+    return optional_present(opt)
+        ? (pred(_optional_val(opt)) ? opt : optional_none(optional_element_t))
+        : optional_none(optional_element_t);
+}
+
+#endif // optional_element_t
+#undef optional_element_t
