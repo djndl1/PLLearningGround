1ce8c50a01f97ad0af5115f6f70fb9e5249212b3
djndl1 <djndl1@gmail.com>
2024-04-10 23:13:48 +0800

    learn(c/macro): macro expands before passing as arguments

--- c/src/macro_def.c
+++ c/src/macro_def.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <assert.h>
 
 #define TEST_PRE TEST_POST
 
+#define EXPANDABLE 1
+#define CONCAT(a, b) a ## b
+
+
 int main()
 {
+    const int CONCAT(EXPANDABLE, _2) = 1;
+    static_assert(EXPANDABLE_2 == 1, "");
+
 #define TEST_POST 1
     printf("TEST_PRE %d\n", TEST_PRE);
 #undef TEST_POST
