5eb98ca8a3e11047b31a79ea976a05cca9fdd7ec
djndl1 <djndl1@gmail.com>
2024-05-30 22:19:31 +0800

    feat(msbuild): add PackageReference
    Use nuget restore before msbuild

--- msbuild_csharp/Main.cs
+++ msbuild_csharp/Main.cs
@@ -6,11 +6,19 @@ using System.Linq;
 
 namespace msbuild_csharp
 {
+    class InitExample
+    {
+        public int Member { get; init; }
+    }
+
     class Program
     {
         static int Main(string[] args)
         {
-            Console.WriteLine("Test");
+            Console.WriteLine(string.Concat(new string[] { "Test", " Test" }));
+            var (a, b) = (2, 2);
+
+            InitExample eg = new InitExample { Member = 5 };
 
             return 0;
         }
