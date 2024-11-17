7df2a8bb7504ec841985b6396a973b79cb7e9921
djndl1 <djndl1@gmail.com>
2024-11-17 11:35:16 +0800

    refactor(c/optional): add match statement macros

--- c/src/optional.c
+++ c/src/optional.c
@@ -48,24 +48,31 @@ int main(void)
     optional(bool) found = optional_map_to(bool, result, as_bool);
     printf(optional_present(found) ? "Found\n" : "Not found\n");
 
-    optional_match(result,
-                   size_t, found_idx_opt,
-        scoped_stmt(
-            printf("Matched\n"),
-            printf("Found 1 at %zu\n", found_idx_opt)
-        ),
-            printf("Not found")
-    );
+    match_begin(optional(size_t), result) {
+        on_some_begin (size_t, found_idx_opt) {
+            printf("Matched\n");
+            printf("Found 1 at %zu\n", found_idx_opt);
+        } on_some_end
+
+        on_none_begin {
+            printf("Not found");
+        } on_none_end
+    } match_end
 
     optional(size_t) filtered = optional_filter(size_t)(result, is_odd);
     assert(optional_present(filtered));
 
     optional(size_t) found_none = index_of(f, 8);
-    optional_match(found_none,
-                   size_t, _,
-            printf("Matched\n"),
-            printf("Not found\n")
-        );
+
+    match_begin (optional(size_t), found_none) {
+        on_some_begin (size_t, _) {
+            printf("Matched\n");
+        } on_some_end
+
+        on_none_begin {
+            printf("Not found\n");
+        } on_none_end
+    } match_end
     assert(optional_or_else(found_none, 10) == 10);
 
     return 0;
--- c/src/optional.h
+++ c/src/optional.h
@@ -26,14 +26,19 @@
 #define optional_none(T) ((optional(T)){ ._present = false })
 #define optional_element_predicate(T) CONCAT(optional(T), _element_predicate_t)
 
-#define optional_match(opt, typ, val_var, matched_stmt, empty_stmt) do { \
-        if (optional_present(opt)) {                                    \
-            typ val_var = _optional_val(opt);                           \
-            scoped_stmt(matched_stmt);                                  \
-        } else {                                                        \
-            empty_stmt;                                                \
-                }                                                      \
-    } while (false)
+#define match_begin(typ, match_obj)  {                              \
+    typ __ca3ecda3_7219_428c_8ea3_cebaff20be9b = match_obj; \
+
+#define on_some_begin(typ, val) {                                          \
+    typ val = _optional_val(__ca3ecda3_7219_428c_8ea3_cebaff20be9b);           \
+    if (optional_present(__ca3ecda3_7219_428c_8ea3_cebaff20be9b))
+
+#define on_some_end }
+
+#define on_none_begin {                                    \
+    if (!optional_present(__ca3ecda3_7219_428c_8ea3_cebaff20be9b))
+#define on_none_end }
+#define match_end }
 
 #define _optional_val(opt) (opt._hidden_obj.t_value)
 
