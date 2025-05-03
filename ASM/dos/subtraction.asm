f100b37071e360832bb4bd5b95af353b31ff12e3
djndl1 <djndl1@gmail.com>
2025-05-03 22:51:09 +0800

    refactor(asm/dos): make multiword subtraction a function

--- ASM/dos/subtraction.asm
+++ ASM/dos/subtraction.asm
@@ -14,20 +14,34 @@ main    proc    far
     mov ax, @DATA
     mov ds, ax
 
-    mov ax, word ptr [data1]
-    sub ax, word ptr [data2]
-
-    mov word ptr [result], ax
-
-    mov ax, word ptr [data1 + 2]
-    sbb ax, word ptr [data2 + 2]
-
-    mov word ptr [result + 2], ax
+    push    word ptr [data2+2]
+    push    word ptr [data2]
+    push    word ptr [data1+2]
+    push    word ptr [data1]
+    call    _dwordsub
+    add     sp, 8
+    mov     word ptr [result], ax
+    mov     word ptr [result+2], dx
 
 ;; exit
     mov ah, 4Ch
     int 21H
 
 main    endp
+
+    ;; int32_t dwordsub(int32_t a, int32_t b)
+_dwordsub    proc    near
+    push    bp
+    mov     bp, sp
+
+    mov     ax, word ptr [bp+4]
+    mov     dx, word ptr [bp+6]
+    clc
+    sub     ax, word ptr [bp+8]
+    sbb     dx, word ptr [bp+10]
+
+    pop     bp
+    ret
+_dwordsub   endp
 
 end main
