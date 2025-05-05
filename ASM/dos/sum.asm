887041001bc5e2b6faedb5579767d7c3d0b94c54
djndl1 <djndl1@gmail.com>
2025-05-06 00:13:22 +0800

    learn(asm/dos): my first x86 floating-point addition

--- ASM/dos/sum.asm
+++ ASM/dos/sum.asm
@@ -8,24 +8,52 @@
 nums    dw  2300,4300,1200,3700,1298,4323,5673,986
 sums    dw  0
 
+fnums   dd  1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.1
+fsums   dd  0.0
+
 .code
 
 ;;;  int16_t sum(int16_t const *nums, uint16_t len)
 
-_sum proto   near, pnums:word, len:word
+sum proto   near, pnums:word, len:word
+fsum proto  near, pnums:word, len:word
 
 .startup
 main    proc    far
     ;; parameter passing code is generated
     lea     ax, [nums]
-    invoke    _sum, ax, 8
+    invoke    sum, ax, 8
     ;; stack cleanup is generated
     mov     word ptr [sums], ax
+
+    mov     ax, offset fnums
+    invoke  fsum, ax, 8
+    fstp    dword ptr [fsums]
 main    endp
 .exit
 
+fsum proc  near, pnums:word, len:word
+    push    si
+
+    mov     cx, word ptr [bp+6]
+    mov     si, word ptr [bp+4]
+
+    finit
+    fldz
+begin_add:
+    fld     dword ptr [si]
+    faddp   st(1), st(0)
+    add     si, 4
+    loop    begin_add
+end_add:
+
+    pop     si
+    ;; pop bp is generated automatically
+    ret
+fsum endp
+
     ;; prolog and epilog are generated
-_sum proc    near, pnums:word, len:word
+sum proc    near, pnums:word, len:word
     push    si
 
     mov     cx, word ptr [bp+6]
@@ -41,6 +69,6 @@ end_add:
 
     ;; pop bp is generated automatically
     ret
-_sum endp
+sum endp
 
 end
